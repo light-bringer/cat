95bb44b8950d73ea8e341ce163163764bb1525e4
husam88 <husam.n.yas@gmail.com>
2018-05-09 14:27:31 +0800

    Add files via upload

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/vagrant/subjects/tool_subjects/dynodroid/com.everysoft.autoanswer_6_src/bin/AndroidManifest.xml : \
+/vagrant/subjects/tool_subjects/dynodroid/com.everysoft.autoanswer_6_src/AndroidManifest.xml \
---
+++ bin/AutoAnswerPreferenceActivity-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+/vagrant/subjects/tool_subjects/dynodroid/com.everysoft.autoanswer_6_src/bin/AutoAnswerPreferenceActivity-debug-unaligned.apk : \
+/vagrant/subjects/tool_subjects/dynodroid/com.everysoft.autoanswer_6_src/bin/AutoAnswerPreferenceActivity.ap_ \
+/vagrant/subjects/tool_subjects/dynodroid/com.everysoft.autoanswer_6_src/bin/classes.dex \
+/home/vagrant/android-sdk-linux/tools/support/annotations.jar \
+/home/vagrant/android-sdk-linux/tools/lib/emma_device.jar \
